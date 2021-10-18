c1900c603378418cbad4ea3a39e1ee36a005ff4e
xshady <fastik036@gmail.com>
2021-10-18 23:07:40 +0300

    add players all meta clearing on resourceStop

--- src/alt-overwrite-banner.js
+++ src/alt-overwrite-banner.js
@@ -2,14 +2,18 @@ import alt from 'alt-server'
 
 (() => {
   const BaseObject = alt.BaseObject
+  const Player = alt.Player
   const baseObjects = new Set()
+
+  const clearPlayersMeta = overwritePlayerMetaMethods(Player)
 
   for (const key in alt) {
     const baseObjectChild = alt[key]
 
     if (!(
-      baseObjectChild.prototype instanceof BaseObject &&
-      baseObjectChild !== BaseObject
+      baseObjectChild !== BaseObject &&
+      baseObjectChild !== Player &&
+      baseObjectChild.prototype instanceof BaseObject
     )) continue
 
     alt[key] = class extends baseObjectChild {
@@ -42,6 +46,56 @@ import alt from 'alt-server'
     // alt.log('resourceStop baseobjects:', baseObjects.size)
     for (const obj of baseObjects) {
       obj.destroy()
+      clearPlayersMeta()
     }
   })
+
+  function overwritePlayerMetaMethods (Player) {
+    const proto = Player.prototype
+
+    const metaStoreKey = Symbol('metaStoreKey')
+    const syncedMetaStoreKey = Symbol('syncedMetaStoreKey')
+    const streamSyncedMetaStoreKey = Symbol('streamSyncedMetaStoreKey')
+
+    const originalSetMeta = Symbol('originalSetMeta')
+    const originalSetSyncedMeta = Symbol('originalSetSyncedMeta')
+    const originalSetStreamSyncedMeta = Symbol('originalSetStreamSyncedMeta')
+
+    proto[originalSetMeta] = proto.setMeta
+    proto[originalSetSyncedMeta] = proto.setSyncedMeta
+    proto[originalSetStreamSyncedMeta] = proto.setStreamSyncedMeta
+
+    const defineMetaSetter = (originalMethodKey, storeKey) =>
+      function (key, value) {
+        this[originalMethodKey](key, value)
+
+        this[storeKey] = this[storeKey] || {}
+        this[storeKey][key] = value
+      }
+
+    proto.setMeta = defineMetaSetter(originalSetMeta, metaStoreKey)
+    proto.setSyncedMeta = defineMetaSetter(originalSetSyncedMeta, syncedMetaStoreKey)
+    proto.setStreamSyncedMeta = defineMetaSetter(originalSetStreamSyncedMeta, streamSyncedMetaStoreKey)
+
+    return () => {
+      const players = alt.Player.all
+
+      for (let i = 0; i < players.length; i++) {
+        const player = players[i]
+
+        for (const key in player[metaStoreKey]) {
+          // alt.log('deleteMeta', key)
+          player.deleteMeta(key)
+        }
+
+        for (const key in player[syncedMetaStoreKey]) {
+          player.deleteSyncedMeta(key)
+        }
+
+        for (const key in player[streamSyncedMetaStoreKey]) {
+          player.deleteStreamSyncedMeta(key)
+        }
+      }
+    }
+  }
 })()
